5abdf7055ed887088c532d2dd956cdc259587711
Dag Ohlin <ohlin.dag@gmail.com>
2025-05-12 22:47:48 +0200

    meh

--- Tests/minimal.s
+++ Tests/minimal.s
@@ -1,6 +1,11 @@
-    .global myFunction
+.global myFunction
 
 myFunction:
-    movq %rdi, %rax   # Move the input from rdi to rax (return value register)
-    addq %rax, %rax    # Multiply the input by 2
+    pushq %rbp        # Save the base pointer
+    movq %rsp, %rbp   # Set the base pointer
+
+    movq %rdi, %rax   # Move the input from rdi to rax
+    addq %rax, %rax   # Multiply the input by 2
+
+    popq %rbp         # Restore the base pointer
     ret
